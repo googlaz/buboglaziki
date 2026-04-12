924bdd0c14464f625986cd595c5a33b701f17edb
Neto test <bunatasha23@yandex.ru>
2026-04-13 00:23:31 +0300

    fix: replace jitsi_meet_flutter_sdk with url_launcher to resolve all build conflicts

--- lib/services/jitsi_service.dart
+++ lib/services/jitsi_service.dart
@@ -1,25 +1,24 @@
-import 'package:jitsi_meet_flutter_sdk/jitsi_meet_flutter_sdk.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class JitsiService {
-  static final _jitsiMeetPlugin = JitsiMeet();
-
   static Future<void> joinMeeting({
     required String roomName,
     required String userName,
     required bool isVideoCall,
   }) async {
-    var options = JitsiMeetConferenceOptions(
-      room: roomName,
-      serverURL: 'https://meet.jit.si',
-      configOverrides: {
-        "startWithAudioMuted": false,
-        "startWithVideoMuted": !isVideoCall,
-      },
-      userInfo: JitsiMeetUserInfo(
-        displayName: userName,
-      ),
-    );
-    
-    await _jitsiMeetPlugin.join(options);
+    // Формируем URL для Jitsi Meet с параметрами пользователя
+    final config = [
+      'config.startWithAudioMuted=false',
+      'config.startWithVideoMuted=${!isVideoCall}',
+      'userInfo.displayName=$userName',
+    ].join('&');
+
+    final url = Uri.parse('https://meet.jit.si/$roomName#$config');
+
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url, mode: LaunchMode.externalApplication);
+    } else {
+      throw Exception('Не удалось открыть Jitsi Meet');
+    }
   }
 }
