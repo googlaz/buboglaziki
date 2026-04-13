4df63a5385b9b1e34b725f44573a365a64aecfa7
Neto test <bunatasha23@yandex.ru>
2026-04-13 21:55:13 +0300

    feat: native Jitsi SDK for in-app audio and video calls (no browser redirect)

--- lib/services/jitsi_service.dart
+++ lib/services/jitsi_service.dart
@@ -1,26 +1,29 @@
-import 'package:url_launcher/url_launcher.dart';
+import 'package:jitsi_meet_flutter_sdk/jitsi_meet_flutter_sdk.dart';
 
 class JitsiService {
+  static final _jitsiMeetPlugin = JitsiMeet();
+
   static Future<void> joinMeeting({
     required String roomName,
     required String userName,
     required bool isVideoCall,
   }) async {
-    // Формируем URL для Jitsi Meet с параметрами пользователя
-    final config = [
-      'config.startWithAudioMuted=false',
-      'config.startWithVideoMuted=${!isVideoCall}',
-      'config.startAudioOnly=${!isVideoCall}',
-      'config.disableDeepLinking=true',
-      'userInfo.displayName=$userName',
-    ].join('&');
+    var options = JitsiMeetConferenceOptions(
+      room: roomName,
+      serverURL: 'https://meet.jit.si',
+      configOverrides: {
+        "startWithAudioMuted": false,
+        "startWithVideoMuted": !isVideoCall,
+        "startAudioOnly": !isVideoCall,
+      },
+      featureFlags: {
+        "unsaferoomwarning.enabled": false,
+      },
+      userInfo: JitsiMeetUserInfo(
+        displayName: userName,
+      ),
+    );
 
-    final url = Uri.parse('https://meet.jit.si/$roomName#$config');
-
-    if (await canLaunchUrl(url)) {
-      await launchUrl(url, mode: LaunchMode.externalApplication);
-    } else {
-      throw Exception('Не удалось открыть Jitsi Meet');
-    }
+    await _jitsiMeetPlugin.join(options);
   }
 }
